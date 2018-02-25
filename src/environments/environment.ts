709ca8809d471f4f91c5423fc91aa8c469dbbd4b
INORIA <inoria@gfb.io>
2018-02-25 10:45:19 +0900

    Remove firebase config just because

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
+import firebase from './firebase';
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
@@ -6,12 +8,5 @@
 export const environment = {
   production: false,
   hmr: false,
-  firebase: {
-    apiKey: "AIzaSyD7uhvcBW7tgp0-iBMM98otuCdUj3Oot00",
-    authDomain: "angularfire-db7be.firebaseapp.com",
-    databaseURL: "https://angularfire-db7be.firebaseio.com",
-    projectId: "angularfire-db7be",
-    storageBucket: "angularfire-db7be.appspot.com",
-    messagingSenderId: "327571334583"
-  }
+  firebase
 };
