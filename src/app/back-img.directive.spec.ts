e44f4a0d8b620bc4b90fb609d228853a556475e1
INORIA <inoria@gfb.io>
2018-02-28 01:49:12 +0900

    :white_check_mark: Add back-img directive test

--- src/app/back-img.directive.spec.ts
+++ src/app/back-img.directive.spec.ts
@@ -1,8 +1,30 @@
+import { Component, DebugElement } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BackImgDirective } from './back-img.directive';
 
-describe('BackImgDirective', () => {
+@Component({
+  template: `<div appBackImg="http://example.com/image.png"></div>`
+})
+class TestBackgroundComponent {}
+
+describe('Directive: BackImgDirective', () => {
+  let component: TestBackgroundComponent;
+  let fixture: ComponentFixture<TestBackgroundComponent>;
+  let divEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestBackgroundComponent, BackImgDirective ]
+    })
+
+    fixture = TestBed.createComponent(TestBackgroundComponent);
+    component = fixture.debugElement.componentInstance;
+    divEl = fixture.debugElement.query(By.css('div'));
+  });
+
   it('should create an instance', () => {
-    const directive = new BackImgDirective();
-    expect(directive).toBeTruthy();
+    fixture.detectChanges();
+    expect(divEl.nativeElement.style.backgroundImage).toBe('url("http://example.com/image.png")');
   });
 });
