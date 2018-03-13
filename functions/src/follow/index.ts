b988fe4ca03b14252cd2545f14549d2060556da1
INORIA <inoria@gfb.io>
2018-03-13 23:17:35 +0900

    :sparkles: Implement follow cloud function #12

--- functions/src/follow/index.ts
+++ functions/src/follow/index.ts
@@ -2,25 +2,38 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 import { bindApp } from '../common/bind-app';
 import { validateFirebaseIdToken } from '../common/validate-firebase-id-token';
 
 const app = express();
 const corsConfig = cors({origin: 'http://localhost:4200'});
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(corsConfig);
 app.use(validateFirebaseIdToken);
 
 app.post('*', async (req: express.Request, res: express.Response) => {
-  return res.json({
-    user: req['user']
-  });
+  const userId = req['user'].uid;
+  const characterId = req.body.characterId;
 
-  // return admin.firestore().collection('character_follows').add({
-  //   name: 'ooeijawfoiej'
-  // }).then((ref) => {
-  //   res.send("Hello from Firebase!" + ref.id);
-  // });
+  try {
+    await admin
+      .firestore()
+      .collection('character_followers')
+      .doc(characterId)
+      .set({ [userId]: true }, { merge: true });
+
+    await admin
+      .firestore()
+      .collection('user_follows')
+      .doc(userId)
+      .set({ [characterId]: true }, { merge: true });
+
+    return res.json({});
+  } catch(e) {
+    return res.status(500).json({});
+  }
 });
 
 export const followCharacterFunc = bindApp(app);
