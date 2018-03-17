76d115a7b05ab80d28d84e5de850c50a7eed8798
INORIA <inoria@gfb.io>
2018-03-18 00:20:54 +0900

    :green_heart: Fix firebase auth-invalid error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
-import firebase from './firebase';
-
 export const environment = {
   production: true,
   hmr: false,
-  firebase
+  firebase: {
+    apiKey: "AIzaSyD7uhvcBW7tgp0-iBMM98otuCdUj3Oot00",
+    authDomain: "angularfire-db7be.firebaseapp.com",
+    databaseURL: "https://angularfire-db7be.firebaseio.com",
+    projectId: "angularfire-db7be",
+    storageBucket: "angularfire-db7be.appspot.com",
+    messagingSenderId: "327571334583"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
-import firebase from './firebase';
-
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
@@ -8,5 +6,12 @@ import firebase from './firebase';
 export const environment = {
   production: false,
   hmr: false,
-  firebase
+  firebase: {
+    apiKey: "AIzaSyD7uhvcBW7tgp0-iBMM98otuCdUj3Oot00",
+    authDomain: "angularfire-db7be.firebaseapp.com",
+    databaseURL: "https://angularfire-db7be.firebaseio.com",
+    projectId: "angularfire-db7be",
+    storageBucket: "angularfire-db7be.appspot.com",
+    messagingSenderId: "327571334583"
+  }
 };
