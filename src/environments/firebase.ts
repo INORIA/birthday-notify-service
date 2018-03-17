913732e606d183639d510ea9163f0981b63f52ed
INORIA <inoria@gfb.io>
2018-03-17 19:25:43 +0900

    :up: Add firebase config

---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyD7uhvcBW7tgp0-iBMM98otuCdUj3Oot00",
+  authDomain: "angularfire-db7be.firebaseapp.com",
+  databaseURL: "https://angularfire-db7be.firebaseio.com",
+  projectId: "angularfire-db7be",
+  storageBucket: "angularfire-db7be.appspot.com",
+  messagingSenderId: "327571334583"
+};
