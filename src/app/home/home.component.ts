fcc0059be3ff7a8c4ebc034038bab11b80715f6a
INORIA <inoria@gfb.io>
2018-03-21 01:26:07 +0900

    :sparkles: add character list of birthday this month #20

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,9 @@ export class HomeComponent implements OnInit {
 
   private characterCollection: AngularFirestoreCollection<any>;
   characters: Observable<any[]>;
+  birthThisMonth: Observable<any[]>;
 
-  constructor(db: AngularFirestore, public afAuth: AngularFireAuth) {
+  constructor(private db: AngularFirestore, public afAuth: AngularFireAuth) {
     this.characterCollection = db.collection('characters');
     this.characters = this.characterCollection.snapshotChanges().map(actions => {
       return actions.map(a => {
@@ -23,6 +24,16 @@ export class HomeComponent implements OnInit {
         return { _id, ...data };
       });
     });;
+
+    const thisMonth = '' + (new Date().getMonth() + 1);
+    this.birthThisMonth = this.db.collection('characters', ref => ref.where('birthday_month', '==', thisMonth))
+      .snapshotChanges().map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const _id = a.payload.doc.id;
+          return { _id, ...data };
+        });
+      });
    }
 
   ngOnInit() {
