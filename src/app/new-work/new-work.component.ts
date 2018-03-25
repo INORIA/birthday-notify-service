122732beb881d41ef54c8ff96a04126f0810527b
INORIA <inoria@gfb.io>
2018-03-25 23:17:40 +0900

    :sparkles: implement work submit feature #25

--- src/app/new-work/new-work.component.ts
+++ src/app/new-work/new-work.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { DocumentReference } from "@firebase/firestore-types";
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+
+import { IWork, Work } from '../models/work';
+import { FormStates } from '../enums/form-states';
+
 
 @Component({
   selector: 'app-new-work',
@@ -7,9 +16,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewWorkComponent implements OnInit {
 
-  constructor() { }
+  FormStates = FormStates;
+  formState = FormStates.WaitForSubmit;
+  model = new Work();
+  submitPercent: number = 0;
+
+  constructor(
+    private afs: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  async onSubmit(e) {
+    this.formState = FormStates.Submitting;
+
+    try {
+      const user = this.afAuth.auth.currentUser;
+      await this.postData({
+        category: this.model.category,
+        name: this.model.name
+      });
+      this.submitPercent = 100;
+      this.formState = FormStates.Submitted;
+
+      setTimeout(() => {
+        location.reload();
+      }, 600);
+    } catch (e) {
+      this.message(`${this.model.name} を作成できませんでした。`);
+      console.log(e.message);
+    }
+  }
+
+  private postData(data: IWork): Promise<DocumentReference> {
+    return this.afs.collection<IWork>('works').add(data);
+  }
+
+  private message(message: string) {
+    const config = new MatSnackBarConfig();
+    config.duration = 2000;
+    this.snackBar.open(message, '', config);
   }
 
 }
