f2d40b5fe3890533d34eb1557df241264965d365
INORIA <inoria@gfb.io>
2018-03-26 22:20:19 +0900

    :sparkles: load categories from db #25

--- src/app/new-work/new-work.component.ts
+++ src/app/new-work/new-work.component.ts
@@ -4,9 +4,11 @@ import { DocumentReference } from "@firebase/firestore-types";
 import { AngularFireStorage } from 'angularfire2/storage';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 
 import { IWork, Work } from '../models/work';
 import { FormStates } from '../enums/form-states';
+import { ICategory } from '../models/category';
 
 
 @Component({
@@ -20,12 +22,22 @@ export class NewWorkComponent implements OnInit {
   formState = FormStates.WaitForSubmit;
   model = new Work();
   submitPercent: number = 0;
+  categories: Observable<ICategory[]>;
 
   constructor(
     private afs: AngularFirestore,
     private snackBar: MatSnackBar,
     private afAuth: AngularFireAuth
-  ) { }
+  ) {
+    const categoriesCollection = afs.collection<ICategory>('categories');
+    this.categories = categoriesCollection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as ICategory;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+  }
 
   ngOnInit() {
 
