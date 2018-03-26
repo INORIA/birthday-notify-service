e5764ad5d5d3650adb966861758bdcf704a619dd
INORIA <inoria@gfb.io>
2018-03-27 01:41:03 +0900

    :sparkles: add cron function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,10 @@ import * as admin from 'firebase-admin';
 
 import { followCharacterFunc } from './follow';
 import { sendEmailFunc } from './send-mail';
+import { cronTwitterTodayBirthdayFunc } from './cron-twitter-today-birthday';
 
 admin.initializeApp(functions.config().firebase);
 
 export const followCharacter = followCharacterFunc;
 export const sendEmail = sendEmailFunc;
+export const cronTwitterTodayBirthday = cronTwitterTodayBirthdayFunc;
