f7d29e5c58c6bf29c3a9dce3861af3ac9ec9930a
INORIA <inoria@gfb.io>
2018-03-28 00:17:27 +0900

    :sparkles: adjust console log of cf

--- functions/src/cron-twitter-today-birthday/index.ts
+++ functions/src/cron-twitter-today-birthday/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-export const cronTwitterTodayBirthday = functions.pubsub.topic('hourly-tick').onPublish(async event => {
+export const cronTwitterTodayBirthdayFunc = functions.pubsub.topic('hourly-tick').onPublish(async event => {
   const date = new Date();
   const thisDate = '' + date.getDate();
   const thisMonth = '' + (date.getMonth() + 1);
 
   const birthToday = await admin.firestore().collection('characters').where('birthday_month', '==', thisMonth).where('birthday_date', '==', thisDate).get();
-  console.log(birthToday);
+  birthToday.forEach(c => {
+    console.log(c.data());
+  });
 });
