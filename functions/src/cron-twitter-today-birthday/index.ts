6ce28d2a5976bf8b41b32dd8b8a2e5470c3887f7
INORIA <inoria@gfb.io>
2018-03-30 23:08:20 +0900

    :sparkles: add tweet processing

--- functions/src/cron-twitter-today-birthday/index.ts
+++ functions/src/cron-twitter-today-birthday/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as Twitter from 'twitter';
 
 export const cronTwitterTodayBirthdayFunc = functions.pubsub.topic('hourly-tick').onPublish(async event => {
   const date = new Date();
@@ -9,5 +10,23 @@ export const cronTwitterTodayBirthdayFunc = functions.pubsub.topic('hourly-tick'
   const birthToday = await admin.firestore().collection('characters').where('birthday_month', '==', thisMonth).where('birthday_date', '==', thisDate).get();
   birthToday.forEach(c => {
     console.log(c.data());
+
+    try {
+      const client = new Twitter({
+        consumer_key: functions.config().twitter.consumer_key,
+        consumer_secret: functions.config().twitter.consumer_secret,
+        access_token_key: functions.config().twitter.access_token_key,
+        access_token_secret: functions.config().twitter.access_token_secret
+      });
+
+      const data = c.data();
+      const body = `${data.name}`;
+
+      client.post('statuses/update', {status: body}).catch(e => {
+        console.log(e);
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
   });
 });
